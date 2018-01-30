cf932ccddfe48290d16c31674fe5095255188cc6
ashish-jabble <ashish@dianomic.com>
2018-01-30 18:45:51 +0530

    audit log codes endpoint with mocking

--- tests/unit/python/foglamp/services/core/api/test_audit.py
+++ tests/unit/python/foglamp/services/core/api/test_audit.py
@@ -7,8 +7,12 @@
 
 import json
 import pytest
-from foglamp.services.core import routes
 from aiohttp import web
+from unittest.mock import MagicMock, patch
+from foglamp.services.core import routes
+from foglamp.services.core import connect
+from foglamp.common.storage_client.storage_client import StorageClient
+
 
 __author__ = "Ashish Jabble"
 __copyright__ = "Copyright (c) 2017 OSIsoft, LLC"
@@ -20,52 +24,19 @@ __version__ = "${VERSION}"
 @pytest.allure.story("audit")
 class TestAudit:
 
-    def create_app(self, loop):
-        app = web.Application(loop=loop)
-        # fill the routes table
-        routes.setup(app)
-        return app
-
-    async def test_get_severity(self, test_client):
-        client = await test_client(self.create_app)
-        resp = await client.get('/foglamp/audit/severity')
-        assert resp.status == 200
-        result = await resp.text()
-        result_dict = json.loads(result)
-        log_severity = result_dict['logSeverity']
-
-        # verify the severity count
-        assert 4 == len(log_severity)
-
-        # verify the name and value of severity
-        for i in range(len(log_severity)):
-            if log_severity[i]['index'] == 1:
-                assert 1 == log_severity[i]['index']
-                assert 'FATAL' == log_severity[i]['name']
-            elif log_severity[i]['index'] == 2:
-                assert 2 == log_severity[i]['index']
-                assert 'ERROR' == log_severity[i]['name']
-            elif log_severity[i]['index'] == 3:
-                assert 3 == log_severity[i]['index']
-                assert 'WARNING' == log_severity[i]['name']
-            elif log_severity[i]['index'] == 4:
-                assert 4 == log_severity[i]['index']
-                assert 'INFORMATION' == log_severity[i]['name']
-
-    # Via Fixture
     @pytest.fixture
-    def cli(self, loop, test_client):
+    def client(self, loop, test_client):
         app = web.Application(loop=loop)
         # fill the routes table
         routes.setup(app)
         return loop.run_until_complete(test_client(app))
 
-    async def test_get_severity_via_fixture(self, cli):
-        resp = await cli.get('/foglamp/audit/severity')
+    async def test_get_severity(self, client):
+        resp = await client.get('/foglamp/audit/severity')
         assert resp.status == 200
         result = await resp.text()
-        result_dict = json.loads(result)
-        log_severity = result_dict['logSeverity']
+        json_response = json.loads(result)
+        log_severity = json_response['logSeverity']
 
         # verify the severity count
         assert 4 == len(log_severity)
@@ -85,13 +56,26 @@ class TestAudit:
                 assert 4 == log_severity[i]['index']
                 assert 'INFORMATION' == log_severity[i]['name']
 
+    async def test_audit_log_codes(self, client):
+        storage_client_mock = MagicMock(StorageClient)
+        response = [{"code": "PURGE", "description": "Data Purging Process"},
+                    {"code": "LOGGN", "description": "Logging Process"},
+                    {"code": "STRMN", "description": "Streaming Process"},
+                    {"code": "SYPRG", "description": "System Purge"}
+                    ]
+        with patch.object(connect, 'get_storage', return_value=storage_client_mock):
+            with patch.object(storage_client_mock, 'query_tbl', return_value={"rows": response}):
+                resp = await client.get('/foglamp/audit/logcode')
+                assert resp.status == 200
+                result = await resp.text()
+                json_response = json.loads(result)
+                log_codes = [key['code'] for key in json_response['logCode']]
 
-# O/p
-"""
-collected 2 items 
+                # verify the default log_codes which are defined in init.sql
+                assert 4 == len(log_codes)
 
-test_audit.py::TestAudit::test_get_severity[pyloop] PASSED
-test_audit.py::TestAudit::test_get_severity_via_fixture[pyloop] PASSED
-
-========== 2 passed in 0.19 seconds ============================
-"""
+                # verify code values
+                assert 'PURGE' in log_codes
+                assert 'LOGGN' in log_codes
+                assert 'STRMN' in log_codes
+                assert 'SYPRG' in log_codes
