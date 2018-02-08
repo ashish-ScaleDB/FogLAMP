6e7ee78fcaf9c0ca3fe835d40a38ed78c85e30f6
ashish-jabble <ashish@dianomic.com>
2018-02-08 19:14:22 +0530

    unit tests added for stats and type handling for update

--- python/foglamp/common/statistics.py
+++ python/foglamp/common/statistics.py
@@ -41,6 +41,12 @@ class Statistics(object):
         Returns:
             None
         """
+        if type(key) is not str:
+            raise TypeError('key must be a string')
+
+        if type(value_increment) is not int:
+            raise ValueError('value must be an integer')
+
         try:
             payload = PayloadBuilder()\
                 .WHERE(["key", "=", key])\
@@ -83,16 +89,3 @@ class Statistics(object):
                     'Unable to update statistics value based on statistics_key %s and value_increment %s, error %s'
                     , key, value_increment, str(ex))
                 raise
-
-# TODO: FOGL-484 Move below commented code to tests directory
-# async def _main():
-#     _storage = StorageClient(core_management_host="0.0.0.0", core_management_port=33881)
-#
-#     _stats = Statistics(_storage)
-#     await _stats.update(key='READINGS', value_increment=10)
-#
-#
-# if __name__ == '__main__':
-#     import asyncio
-#     loop = asyncio.get_event_loop()
-#     loop.run_until_complete(_main())
---
+++ tests/unit/python/foglamp/common/test_statistics.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+# FOGLAMP_BEGIN
+# See: http://foglamp.readthedocs.io/
+# FOGLAMP_END
+
+import asyncio
+from unittest.mock import MagicMock, patch
+import pytest
+from foglamp.common.statistics import Statistics
+from foglamp.common.storage_client.storage_client import StorageClient
+
+
+__author__ = "Ashish Jabble"
+__copyright__ = "Copyright (c) 2017 OSIsoft, LLC"
+__license__ = "Apache 2.0"
+__version__ = "${VERSION}"
+
+
+@pytest.allure.feature("unit")
+@pytest.allure.story("common", "statistics")
+class TestStatistics:
+
+    def test_init_with_storage(self):
+        storage_client_mock = MagicMock(spec=StorageClient)
+        s = Statistics(storage_client_mock)
+        assert isinstance(s._storage, StorageClient)
+
+    def test_init_with_no_storage(self):
+        storage_client_mock = None
+        with pytest.raises(TypeError) as excinfo:
+            Statistics(storage_client_mock)
+        assert 'Must be a valid Storage object' in str(excinfo.value)
+
+    def test_update(self):
+        storage_client_mock = MagicMock(spec=StorageClient)
+        s = Statistics(storage_client_mock)
+        payload = '{"where": {"column": "key", "condition": "=", "value": "READING"}, ' \
+                  '"expressions": [{"column": "value", "operator": "+", "value": 5}]}'
+        result = {"response": "updated", "rows_affected": 1}
+        with patch.object(s._storage, 'update_tbl', return_value=result) as stat_update:
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(s.update('READING', 5))
+            stat_update.assert_called_once_with('statistics', payload)
+            assert "updated" == result['response']
+
+    @pytest.mark.parametrize("key, value_increment, exception_name, exception_message", [
+        (123456, 120, TypeError, "key must be a string"),
+        ('PURGED', '120', ValueError, "value must be an integer"),
+        (None, '120', TypeError, "key must be a string"),
+        ('123456', '120', ValueError, "value must be an integer"),
+        ('READINGS', None, ValueError, "value must be an integer")
+    ])
+    def test_update_with_invalid_params(self, key, value_increment, exception_name, exception_message):
+        storage_client_mock = MagicMock(spec=StorageClient)
+        s = Statistics(storage_client_mock)
+
+        with pytest.raises(exception_name) as excinfo:
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(s.update(key, value_increment))
+
+        assert exception_message == str(excinfo.value)
+
+    # def test_update_exception(self):
+    #     storage_client_mock = MagicMock(spec=StorageClient)
+    #     s = Statistics(storage_client_mock)
+    #     with patch.object(s._storage, 'update_tbl', side_effect=Exception()):
+    #         with pytest.raises(Exception) as excinfo:
+    #             loop = asyncio.get_event_loop()
+    #             loop.run_until_complete(s.update('B', 5))
+    #         print(excinfo)
+    #
+    #     #_logger.assert_called_once_with('abab')
+
+    # def test_add_update(self):
+    #     stat_dict = {'FOGBENCH/TEMPERATURE': 1, 'FOGBENCH/MOUSE': 1, 'FOGBENCH/SWITCH': 1,
+    #                  'FOGBENCH/HUMIDITY': 1, 'FOGBENCH/WALL CLOCK': 1, 'FOGBENCH/PRESSURE': 1}
+    #     storage_client_mock = MagicMock(spec=StorageClient)
+    #     s = Statistics(storage_client_mock)
+    #
+    #     result = {"response": "updated", "rows_affected": 1}
+    #     error = {'retryable': False, 'entryPoint': 'update', 'message': 'No rows where updated'}
+    #     loop = asyncio.get_event_loop()
+    #     # loop.run_until_complete(s.add_update(stat_dict))
+    #     with patch.object(s, 'add_update', side_effect=loop.run_until_complete(s.add_update(stat_dict))):
+    #         with patch.object(s._storage, 'update_tbl', side_effect=result):
+    #             assert "updated" == result['response']
+    #
+    #     # with patch.object(s._storage, 'update_tbl', return_value=error):
+    #     #     with pytest.raises(KeyError) as excinfo:
+    #     #         loop = asyncio.get_event_loop()
+    #     #         loop.run_until_complete(s.add_update(stat_dict))
+    #     #     print("BLAAA----", str(excinfo.value))
